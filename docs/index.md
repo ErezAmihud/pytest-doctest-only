9322cbb3a008c050dc2ac1af45ee87b115d4f2b2
Erez Amihud <erezamihud@gmail.com>
2025-07-30 21:42:34 +0300

    Add all of the code

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Welcome to pytest-doctest-only
+
+A plugin to run only doctest
---
+++ src/pytest_doctest_only/plugin.py
@@ -0,0 +1,15 @@
+import pytest
+from pytest import DoctestItem
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('doctest-only')
+    group.addoption(
+        '--doctest-only',
+        action='store_true',
+        help="Only run doctest tests, don't run anything else."
+    )
+
+def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config, items: list[pytest.Item]):
+    if config.getoption("--doctest-only"):
+        items[:] = [item for item in items if isinstance(item, DoctestItem)]
